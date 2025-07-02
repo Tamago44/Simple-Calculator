d91e8e3495c846315b5060796a6a11afb2c50a11
Godwin <godwintamayo44@gmail.com>
2025-07-02 15:34:35 +0800

    reviewing the code for understanding

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
-document.addEventListener('DOMContentLoaded',function (){
-
-  // Calculator buttons
+document.addEventListener('DOMContentLoaded', function () {
     const equation = document.getElementById('equation');
-    const buttons =document.querySelectorAll('button');
+    const buttons = document.querySelectorAll('button');
 
-    buttons.forEach((btn)=>{
-        btn.addEventListener('click', ()=> handleButtonClick(btn.id,btn.textContent));    /* it will listen the event click from the buttons and it will pass values of each button including the id and textconent to handleButton Click function */
-    });
+    buttons.forEach((btn) => {
+        btn.addEventListener('click', () => handleButtonClick(btn.id, btn.textContent));
 
-    
-    function handleButtonClick(id,value){
-        switch (id){
+    })
+
+    function handleButtonClick(id, value) {
+        switch (id) {
             case 'C':
                 equation.value = '';
                 break;
+
             case 'equals':
-                try{
-                    equation.value = eval(equation.value);
-                }
-                catch{
+                try {
+                    equation.value = new Function(`return ${equation.value}`)();
+
+                } catch {
                     equation.value = 'Error';
                 }
                 break;
+
             case 'percent':
-                equation.value = parseFloat(equation.value)/100;
+                if (equation.value) {
+                    equation.value = parseFloat(equation.value) / 100;
+                }
                 break;
             case 'positiveNegative':
-                equation.value = parseFloat(equation.value)*-1;
+                if (equation.value) {
+                    equation.value = parseFloat(equation.value) * -1;
+                }
                 break;
 
-                default:
-                    // prevent multiple dots
-                    if(id === 'dot' && equation.value.slice(-1) === '.') return;
-                    equation.value +=value.trim();
-                break;   
+            default:
+                if (id === 'dot' && equation.value.slice(-1) === '.') return;
+                equation.value += value.trim();
+                break;
+
         }
+
+
     }
 });
